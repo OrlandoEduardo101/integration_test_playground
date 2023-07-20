3ce6dcb5a7bb360b25e090e9c9624c7bf10bcb83
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2023-07-20 12:30:17 -0300

    MainActivityTest

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -2,3 +2,4 @@ import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() => integrationDriver();
 // flutter drive \ --driver=test_driver/integration_test.dart \ --target=test/main_test.dart
+// ./android/gradlew app:assembleDebug -Ptarget=test/main_test.dart popd
